11a69d6ffbd599eba45b0ed9a138a7f05a665ceb
Jakob Chwastek <chwastek@uni-potsdam.de>
2023-02-07 15:44:06 +0100

    Docs and minor code adjustments
    * Add module docstring
    * Adjust description
    * Add Raises error description
    * Add citation to function docstring
    * Rename threshold to velocity_threshold
    * Remove optional from velocities argument
    * Rewrite positions and velocities matching shape check

--- src/pymovements/events/ivt.py
+++ src/pymovements/events/ivt.py
@@ -1,6 +1,8 @@
+"""
+This module holds the implementation of the ivt algorithm.
+"""
 from __future__ import annotations
 
-from typing import Optional
 import numpy as np
 
 from pymovements.transforms import vnorm
@@ -9,26 +11,42 @@ from pymovements.events import Fixation
 
 def ivt(
         positions: list[list[float]] | np.ndarray,
-        velocities: Optional[list[list[float]] | np.ndarray],
-        threshold: float
+        velocities: list[list[float]] | np.ndarray,
+        velocity_threshold: float
 ) -> list[Fixation]:
     """
     Identification of fixations based on velocity-threshold
 
+    The algorithm classifies each point as a fixation if the velocity is below
+    the given velocity threshold. Consecutive fixation points are merged into
+    one fixation.
+
+    The implementation is based on the description and pseudocode
+    from Salvucci and Goldberg :cite:p:`SalvucciGoldberg2000`.
+
     Parameters
     ----------
-    positions: array-like
+    positions: array-like, shape (N, 2)
         Continuous 2D position time series.
-    velocities: array-like
+    velocities: array-like, shape (N, 2)
         Corresponding continuous 2D velocity time series.
-    threshold: float
-        Velocity threshold.
+    velocity_threshold: float
+        Threshold for a point to be classified as a fixation. If the
+        velocity is below the threshold, the point is classified as a fixation.
 
     Returns
     -------
-    fixations: array
-        List of fixations
+    list[Fixation]:
+        List of Fixation events
 
+    Raises
+    ------
+    ValueError
+        If positions or velocities are None
+        If positions or velocities do not have shape (N, 2)
+        If positions and velocities have different shapes
+        If velocity threshold is None.
+        If velocity threshold is not greater than 0.
     """
     positions = np.array(positions)
     velocities = np.array(velocities)
@@ -47,22 +65,22 @@ def ivt(
         raise ValueError('velocities need to have shape (N, 2)')
 
     # Check matching shape for positions and velocities
-    if not positions.shape == velocities.shape:
+    if positions.shape != velocities.shape:
         raise ValueError(f"shape of positions {positions.shape} doesn't match"
                          f"shape of velocities {velocities.shape}")
 
     # Check if threshold is None
-    if threshold is None:
+    if velocity_threshold is None:
         raise ValueError('velocity threshold is None')
 
     # Check if threshold is greater 0
-    if threshold <= 0:
+    if velocity_threshold <= 0:
         raise ValueError('velocity threshold must be greater than 0')
 
     velocity_norm = vnorm(velocities, axis=1)
 
     # Map velocities lower than threshold to True and greater equals to False
-    fix_map = velocity_norm < threshold
+    fix_map = velocity_norm < velocity_threshold
 
     # Find onsets for group of velocities
     loc_group_onsets = np.empty(len(positions), dtype=bool)
@@ -82,7 +100,8 @@ def ivt(
 
     for onset, offset in fix_groups:
         fixation_points = positions[onset:offset]
-        centroid = tuple(np.mean(fixation_points, axis=0))
+        centroid = np.mean(fixation_points, axis=0, dtype=np.float64)
+        centroid = (centroid[0], centroid[1])
 
         fixations.append(Fixation(onset, offset, centroid))
 
