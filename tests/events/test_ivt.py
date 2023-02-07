6bf026e23883fe3509c94d361023639542815d66
Jakob Chwastek <chwastek@uni-potsdam.de>
2023-02-07 16:12:59 +0100

    add tests for ivt raising errors

--- tests/events/test_ivt.py
+++ tests/events/test_ivt.py
@@ -16,59 +16,113 @@ from pymovements.events.ivt import ivt
             {
                 'positions': None,
                 'velocities': np.ones((100, 2)),
-                'threshold': 1.
+                'velocity_threshold': 1.
             },
             ValueError,
-            id='none_positions_raises_value_error'
+            id='positions_none_raises_value_error'
         ),
         pytest.param(
             {
                 'positions': np.ones((100, 2)),
                 'velocities': None,
-                'threshold': 1.
+                'velocity_threshold': 1.
             },
             ValueError,
-            id='none_velocities_raises_value_error'
+            id='velocities_none_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': 1,
+                'velocities': np.ones((100, 2)),
+                'velocity_threshold': 1.
+            },
+            ValueError,
+            id='positions_not_array_like_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones((100, 2)),
+                'velocities': 1,
+                'velocity_threshold': 1.
+            },
+            ValueError,
+            id='velocities_not_array_like_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones(100),
+                'velocities': np.ones((100, 2)),
+                'velocity_threshold': 1.
+            },
+            ValueError,
+            id='positions_not_2d_array_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones((100, 2)),
+                'velocities': np.ones(100),
+                'velocity_threshold': 1.
+            },
+            ValueError,
+            id='velocities_not_2d_array_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones((100, 3)),
+                'velocities': np.ones((100, 2)),
+                'velocity_threshold': 1.
+            },
+            ValueError,
+            id='positions_not_2_elements_in_second_dimension_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones((100, 2)),
+                'velocities': np.ones((100, 3)),
+                'velocity_threshold': 1.
+            },
+            ValueError,
+            id='velocities_not_2_elements_in_second_dimension_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones((100, 2)),
+                'velocities': np.ones((101, 2)),
+                'velocity_threshold': 1.
+            },
+            ValueError,
+            id='positions_and_velocities_different_lengths_raises_value_error'
         ),
         pytest.param(
             {
                 'positions': np.ones((100, 2)),
                 'velocities': np.ones((100, 2)),
-                'threshold': None
+                'velocity_threshold': None
             },
             ValueError,
-            id='none_threshold_raises_value_error'
+            id='velocity_threshold_none_raises_value_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones((100, 2)),
+                'velocities': np.ones((100, 2)),
+                'velocity_threshold': '1.'
+            },
+            TypeError,
+            id='velocity_threshold_not_float_raises_type_error'
+        ),
+        pytest.param(
+            {
+                'positions': np.ones((100, 2)),
+                'velocities': np.ones((100, 2)),
+                'velocity_threshold': 0.
+            },
+            ValueError,
+            id='velocity_threshold_not_greater_than_0_raises_value_error'
         ),
     ]
 )
 def test_ivt_raise_error(kwargs, expected_error):
+    """Test if ivt raises expected error."""
     with pytest.raises(expected_error):
         ivt(**kwargs)
-
-
-@pytest.mark.parametrize(
-    'kwargs, expected_count',
-    [
-        pytest.param(
-            {
-                'positions': np.ones((100, 2)),
-                'velocities': np.ones((100, 2)),
-                'threshold': 1.5
-            },
-            1,
-            id='constant_velocity_below_threshold_returns_single_fixation'
-        ),
-        pytest.param(
-            {
-                'positions': np.ones((100, 2)),
-                'velocities': np.ones((100, 2)),
-                'threshold': 1.
-            },
-            0,
-            id='constant_velocity_above_threshold_returns_no_fixation'
-        ),
-
-    ]
-)
-def test_ivt_returns_count(kwargs, expected_count):
-    assert len(ivt(**kwargs)) == expected_count
