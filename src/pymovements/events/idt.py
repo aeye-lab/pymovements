3c9462add6907ead783aafc41de1590e0ce2ff24
Jakob Chwastek <chwastek@uni-potsdam.de>
2023-02-07 12:43:39 +0100

    Minor adjustments
    * Rename x to positions
    * Change shape info in error message to (N, 2)
    * Adjust dispersion_threshold greater zero check condition
    * Rewrite centroid calc with np.mean
    * Add Raises Error to docstring
    * Add citation

--- src/pymovements/events/idt.py
+++ src/pymovements/events/idt.py
@@ -5,54 +5,65 @@ import numpy as np
 from pymovements.events import Fixation
 
 
-def dispersion(x):
-    return np.sum(np.max(x, axis=0) - np.min(x, axis=0))
+def dispersion(positions):
+    return np.sum(np.max(positions, axis=0) - np.min(positions, axis=0))
 
 
 def idt(
-        x: list[list[float]] | np.ndarray,
+        positions: list[list[float]] | np.ndarray,
         dispersion_threshold: float,
         duration_threshold: int,
 ) -> list[Fixation]:
     """
     Fixation identification based on dispersion threshold.
 
+    The implementation is based on the description and pseudocode
+    from Salvucci and Goldberg :cite:p:`SalvucciGoldberg2000`.
+
     Parameters
     ----------
-    x: array-like
+    positions: array-like
         Continuous 2D position time series
     dispersion_threshold: float
-        Threshold for dispersion for a group of consecutive points to be identified as fixation
+        Threshold for dispersion for a group of consecutive samples to be identified as fixation
     duration_threshold: int
-        Minimum fixation duration
+        Minimum fixation duration in number of samples
 
     Returns
     -------
     fixations:
         List of Fixation events
+
+    Raises
+    ------
+    ValueError
+        If positions is not shaped (N, 2)
+        If dispersion_threshold is not greater than 0
+        If duration_threshold is not greater than 0
     """
-    x = np.array(x)
+
+    positions = np.array(positions)
 
     # make sure x is 2d
-    if x.ndim != 2:
+    if positions.ndim != 2:
         raise ValueError(
-            'x needs to have shape (n, 2)'
+            'positions needs to have shape (N, 2)'
         )
 
     # make sure x has shape (n, 2)
-    if x.shape[1] != 2:
+    if positions.shape[1] != 2:
         raise ValueError(
-            'x needs to have shape (n, 2)'
+            'positions needs to have shape (N, 2)'
         )
 
     # Check if dispersion_threshold is greater 0
-    if not dispersion_threshold > 0:
+    if dispersion_threshold <= 0:
         raise ValueError(
             'dispersion threshold must be greater than 0'
         )
 
     # Check if duration_threshold is greater 0
-    if not duration_threshold > 0:
+    if duration_threshold <= 0:
         raise ValueError(
             'duration threshold must be greater than 0'
         )
@@ -63,22 +74,19 @@ def idt(
     win_start = 0
     win_end = duration_threshold
 
-    while win_end < len(x):
-        if dispersion(x[win_start:win_end]) <= dispersion_threshold:
+    while win_end < len(positions):
+        if dispersion(positions[win_start:win_end]) <= dispersion_threshold:
             # Add additional points to the window until dispersion > threshold
-            while dispersion(x[win_start:win_end]) < dispersion_threshold:
+            while dispersion(positions[win_start:win_end]) < dispersion_threshold:
                 win_end += 1
 
                 # break if we reach end of input data
-                if win_end == len(x) - 1:
+                if win_end == len(positions) - 1:
                     break
 
             # Note a fixation at the centroid of the window points
-            centroid = np.sum(x[win_start:win_end], axis=0) / len(x[win_start:win_end])
-            onset = win_start
-            offset = win_end
-
-            fixations.append(Fixation(onset, offset, centroid))
+            centroid = tuple(np.mean(positions[win_start:win_end], axis=0))
+            fixations.append(Fixation(win_start, win_end, centroid))
 
             # Initialize new window excluding the previous window
             win_start = win_end
