8aaa9830c9541b2eda38e1c0030bb7b10b072b96
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-02-15 12:21:10 +0100

    Modules: Provide top-level imports via init files

---
+++ src/pymovements/utils/__init__.py
@@ -0,0 +1,6 @@
+"""This module provides access to all utility functions."""
+from pymovements.utils import archives  # noqa: F401
+from pymovements.utils import checks  # noqa: F401
+from pymovements.utils import decorators  # noqa: F401
+from pymovements.utils import downloads  # noqa: F401
+from pymovements.utils import paths  # noqa: F401
