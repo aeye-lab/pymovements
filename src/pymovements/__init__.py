02dce4125402ba7d5363ec36d7e98aed5fa47ed0
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-02-15 13:49:07 +0100

    Modules: Provide top-level imports via init files (#148)

---
+++ src/pymovements/__init__.py
@@ -0,0 +1,5 @@
+"""This module provides access to all submodules."""
+from pymovements import datasets  # noqa: F401
+from pymovements import events  # noqa: F401
+from pymovements import synthetic  # noqa: F401
+from pymovements import utils  # noqa: F401
