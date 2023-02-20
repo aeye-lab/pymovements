44129e6eebd434a4a996be13b2b11d624d99add1
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-02-20 12:07:44 +0100

    Docs: Include Readme file in documentation index (#165)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,6 +63,7 @@ extensions = [
     'sphinx.ext.linkcode',
     'sphinx.ext.mathjax',
     'sphinx.ext.extlinks',
+    'sphinx_mdinclude',
     'sphinx_rtd_theme',
     'sphinx_copybutton',
     'sphinxcontrib.datatemplates',
