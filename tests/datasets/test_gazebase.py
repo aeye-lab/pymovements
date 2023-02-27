6ed07e302f4725a4cc5ed2303078730df3876cf2
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-02-27 12:27:54 +0100

    docstrings

--- tests/datasets/test_gazebase.py
+++ tests/datasets/test_gazebase.py
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Test all functionality in pymovements.datasets.public_dataset."""
+"""Test all functionality in pymovements.datasets.gazebase."""
 from pathlib import Path
 
 import pytest
