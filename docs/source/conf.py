9b29208400a861d37322c6727cb1911da4cf671b
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-02-28 12:16:29 +0100

    Docs: Fix missing logo bug in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -137,8 +137,8 @@ html_theme_options = {
         },
     ],
     'logo': {
-        'image_light': 'logo.svg',
-        'image_dark': 'logo.svg',
+        'image_light': 'https://raw.githubusercontent.com/aeye-lab/pymovements/main/docs/source/_static/logo.svg',  # noqa: E501
+        'image_dark': 'https://raw.githubusercontent.com/aeye-lab/pymovements/main/docs/source/_static/logo.svg',  # noqa: E501
     },
 }
 
