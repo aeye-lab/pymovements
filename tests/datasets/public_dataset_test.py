3077ecd9980c0c06e79e5327ff75d664e17ddee8
theDebbister <deborahnoemie.jakobi@uzh.ch>
2023-03-08 12:45:52 +0100

    tests for public dataset

--- tests/datasets/public_dataset_test.py
+++ tests/datasets/public_dataset_test.py
@@ -18,7 +18,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """Test all functionality in pymovements.datasets.public_dataset."""
+import tarfile
 from pathlib import Path
+from unittest import mock
 
 import pytest
 
@@ -78,3 +80,99 @@ def test_paths(init_kwargs, expected_paths):
     assert dataset.root == expected_paths['root']
     assert dataset.path == expected_paths['path']
     assert dataset.downloads_rootpath == expected_paths['download']
+
+
+def test_dataset_download_fails(tmp_path):
+    class CustomPublicDataset(PublicDataset):
+        _mirrors = ['https://www.example.com/', 'https://www.example.com/']
+        _resources = [
+            {
+                'resource': 'v0.4.0.tar.gz',
+                'filename': 'pymovements-0.4.0.tar.gz',
+                'md5': '52bbf03a7c50ee7152ccb9d357c2bb30',
+            },
+        ]
+
+    with mock.patch(
+            'pymovements.utils.downloads._download_url',
+            side_effect=OSError(),
+    ):
+        with pytest.raises(RuntimeError):
+            CustomPublicDataset(root=tmp_path, download=True)
+
+
+def test_dataset_download_file_not_found(tmp_path):
+    class CustomPublicDataset(PublicDataset):
+        _mirrors = ['https://github.com/aeye-lab/pymovements/archive/refs/tags/']
+        _resources = [
+            {
+                'resource': 'v0.4.0.tar.gz',
+                'filename': 'pymovements-0.4.0.tar.gz',
+                'md5': '52bbf03a7c50ee7152ccb9d357c2bb30',
+            },
+        ]
+
+    with mock.patch(
+            'pymovements.datasets.public_dataset.PublicDataset.download',
+            side_effect=RuntimeError(),
+    ):
+        with pytest.raises(RuntimeError):
+            CustomPublicDataset(root=tmp_path, download=True)
+
+
+def test_dataset_download(tmp_path):
+
+    class CustomPublicDataset(PublicDataset):
+        _mirrors = [
+            'https://github.com/aeye-lab/pymovements/archive/refs/tags/',
+            'https://github.com/aeye-lab/pymovements/archive/refs/tags/',
+        ]
+        _resources = [
+            {
+                'resource': 'v0.4.0.tar.gz',
+                'filename': 'pymovements-0.4.0.tar.gz',
+                'md5': '52bbf03a7c50ee7152ccb9d357c2bb30',
+            },
+        ]
+
+    CustomPublicDataset(root=tmp_path, download=True)
+
+
+@pytest.fixture(
+    name='single_archive',
+    params=[
+        ('gz', 'tar'),
+    ],
+    ids=[
+        'gz_compressed_tar_archive',
+    ],
+)
+def fixture_archive(tmp_path):
+    rootpath = tmp_path
+    compression, extension = 'gz', 'tar'
+
+    # write tmp filepath
+    filepath = rootpath / 'test.file'
+    filepath.write_text('test')
+
+    archive_path = rootpath / f'test.{extension}.{compression}'
+
+    with tarfile.TarFile.open(archive_path, f'w:{compression}') as fp:
+        fp.add(filepath)
+
+    yield archive_path
+
+
+def test_dataset_extract(tmp_path, single_archive):
+
+    class CustomPublicDataset(PublicDataset):
+        _mirrors = []
+        _resources = [
+            {
+                'resource': 'test.tar.gz',
+                'filename': 'test.tar.gz',
+                'md5': '52bbf03a7c50ee7152ccb9d357c2bb30',
+            },
+        ]
+
+    CustomPublicDataset(root=tmp_path, extract=True, dataset_dirname='', downloads_dirname='')
