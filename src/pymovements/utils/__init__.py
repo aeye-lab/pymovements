9a22e7be712287aed0397abc45dcd01cb2bd2df5
SiQube <reich.davidr@gmail.com>
2023-03-14 16:26:13 +0100

    Feature: add `pymovements.utils.asc2csv`
    convert ascii file to csv
    
    tests both created dataframes and sent messages

--- src/pymovements/utils/__init__.py
+++ src/pymovements/utils/__init__.py
@@ -19,6 +19,7 @@
 # SOFTWARE.
 """This module provides access to all utility functions."""
 from pymovements.utils import archives  # noqa: F401
+from pymovements.utils import asc2csv  # noqa: F401
 from pymovements.utils import checks  # noqa: F401
 from pymovements.utils import decorators  # noqa: F401
 from pymovements.utils import downloads  # noqa: F401
