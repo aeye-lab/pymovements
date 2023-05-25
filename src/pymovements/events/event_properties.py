b822896b69ce15d2f5078f7c30acde0a39f6fe76
Daniel Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-05-25 15:46:31 +0200

    completely replae centroid_fixation by position tuple

--- src/pymovements/events/event_properties.py
+++ src/pymovements/events/event_properties.py
@@ -142,65 +142,6 @@ def disposition(position_columns: tuple[str, str] = ('x_pos', 'y_pos')) -> pl.Ex
 
 
 @register_event_property
-def fixation_centroid(
-        component: str = 'x',
-        method: str = 'mean',
-        position_columns: tuple[str, str] = ('x_pos', 'y_pos'),
-) -> pl.Expr:
-    """Calculate a fixations x centroid.
-
-    Parameters
-    ----------
-    component
-        Specify the component of which to calculate the  centroid.
-        Supported components: 'x', 'y'.
-        Defaults to 'x'.
-    method
-        Specify the type of centroid to be extracted, supported methods: 'mean', 'median'.
-        Defaults to 'mean'.
-    position_columns
-        The column names of the pitch and yaw position components.
-
-    Raises
-    ------
-    TypeError
-        If position_columns not of type tuple, position_columns not of length 2, or elements of
-        position_columns not of type str.
-    ValueError
-        If component is not one of the supported methods.
-    ValueError
-        If method is not one of the supported methods.
-    """
-    _check_position_columns(position_columns)
-
-    component_to_pos = {
-        'x': 0,
-        'y': 1,
-    }
-
-    if component not in component_to_pos:
-        raise ValueError(
-            f'Component {component} not supported. '
-            f"Please choose one of the following: ['x', 'y'].",
-        )
-
-    position_component = pl.col(position_columns[component_to_pos[component]])
-    if method not in ['mean', 'median']:
-        raise ValueError(
-            f'Method {method} not supported. '
-            f"Please choose one of the following: ['mean', 'median'].",
-        )
-
-    if method == 'mean':
-        centroid = position_component.mean()
-
-    if method == 'median':
-        centroid = position_component.median()
-
-    return centroid
-
-
-@register_event_property
 def position(
         method: str = 'mean',
         position_column: str = 'position',
