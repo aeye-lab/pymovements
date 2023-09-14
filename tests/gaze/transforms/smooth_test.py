b408d09a500212c4f74cce3e97185be67f049db4
Jakob Chwastek <chwastek@uni-potsdam.de>
2023-09-14 13:32:02 +0200

    Add some tests

--- tests/gaze/transforms/smooth_test.py
+++ tests/gaze/transforms/smooth_test.py
@@ -25,19 +25,84 @@ from polars.testing import assert_frame_equal
 
 import pymovements as pm
 
+
 @pytest.mark.parametrize(
     "kwargs, series, expected_df",
     [
-        (
+        pytest.param(
             {
                 "method": "moving_average",
-                "n_components":  1,
-                "window_length": 3,
-                "padding": 'wrap',
-
+                "n_components": 2,
+                "window_length": 1,
             },
-            pl.Series('position', [[1.,1.],[2.,2.],[3.,3.]], pl.List(pl.Float64)),
-            pl.Series('position', [[1,1],[2,2],[3, 3]], pl.List(pl.Float64)),
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            id="moving_average_window_length_1_returns_same_series",
+        ),
+        pytest.param(
+            {
+                "method": "moving_average",
+                "n_components": 2,
+                "window_length": 3,
+                "padding": None
+            },
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            pl.Series('position', [[None, None], [2., 2.], [None, None]], pl.List(pl.Float64)),
+            id="moving_average_window_length_3_no_padding",
+        ),
+        pytest.param(
+            {
+                "method": "moving_average",
+                "n_components": 2,
+                "window_length": 3,
+                "padding": 0.0
+            },
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            pl.Series('position', [[1., 1.], [2., 2.], [5 / 3, 5 / 3]], pl.List(pl.Float64)),
+            id="moving_average_window_length_3_constant_padding",
+        ),
+        pytest.param(
+            {
+                "method": "moving_average",
+                "n_components": 2,
+                "window_length": 3,
+                "padding": 'nearest'
+            },
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            pl.Series('position', [[4 / 3, 4 / 3], [2., 2.], [8 / 3, 8 / 3]], pl.List(pl.Float64)),
+            id="moving_average_window_length_3_nearest_padding",
+        ),
+        pytest.param(
+            {
+                "method": "moving_average",
+                "n_components": 2,
+                "window_length": 3,
+                "padding": 'mirror'
+            },
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            pl.Series('position', [[5 / 3, 5 / 3], [2., 2.], [7 / 3, 7 / 3]], pl.List(pl.Float64)),
+            id="moving_average_window_length_3_mirror_padding",
+        ),
+        pytest.param(
+            {
+                "method": "exponential_moving_average",
+                "n_components": 2,
+                "window_length": 1,
+            },
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            id="exponential_moving_average_window_length_1_returns_same_series",
+        ),
+        pytest.param(
+            {
+                "method": "savitzky_golay",
+                "n_components": 2,
+                "window_length": 2,
+                "degree": 1,
+            },
+            pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64)),
+            pl.Series('position', [[1.5, 1.5], [2.5, 2.5], [3., 3.]], pl.List(pl.Float64)),
+            id="savitzky_golay_window_length_2_degree_1_returns",
         )
     ]
 )
@@ -50,3 +115,47 @@ def test_smooth_returns(kwargs, series, expected_df):
     )
 
     assert_frame_equal(result_df, expected_df.to_frame())
+
+
+@pytest.mark.parametrize(
+    "kwargs, exception, msg_substrings",
+    [
+        pytest.param(
+            {
+                "method": "invalid_method",
+                "n_components": 2,
+                "window_length": 3,
+            },
+            ValueError,
+            "Unkown method 'invalid_method'. Supported methods are: ",
+            id="invalid_method_raises_value_error",
+        ),
+        pytest.param(
+            {
+                "method": "savitzky_golay",
+                "n_components": 2,
+                "window_length": 3,
+                "degree": None,
+            },
+            TypeError,
+            "'degree' must not be none for method 'savitzky_golay'",
+            id="savitzky_golay_degree_none_raises_type_error",
+        ),
+    ]
+)
+def test_smooth_init_raises_error(kwargs, exception, msg_substrings):
+    """Test if smooth init raises the expected error."""
+    with pytest.raises(exception) as excinfo:
+        pm.gaze.transforms.smooth(**kwargs)
+
+    msg, = excinfo.value.args
+    for msg_substring in msg_substrings:
+        assert msg_substring.lower() in msg.lower()
+
+
+def test_identity_returns_same_series():
+    """Test if identity returns the same series."""
+    series = pl.Series('position', [[1., 1.], [2., 2.], [3., 3.]], pl.List(pl.Float64))
+    series_identity = pm.gaze.transforms._identity(series)
+
+    assert_frame_equal(series.to_frame(), series_identity.to_frame())
