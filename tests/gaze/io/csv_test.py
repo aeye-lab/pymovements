0c53ecd2e4efae9bd5f438371856d4cbde49662b
prassepaul <paul.prasse@uni-potsdam.de>
2023-09-29 18:29:21 +0200

    added dataset test files and tests

--- tests/gaze/io/csv_test.py
+++ tests/gaze/io/csv_test.py
@@ -44,6 +44,69 @@ import pymovements as pm
             (10, 3),
             id='csv_bino_shape',
         ),
+        pytest.param(
+            {
+                'file': 'tests/gaze/io/files/hbn_example.csv',
+                'time_column': 'time',
+                'pixel_columns': ['x_pix', 'y_pix'],
+            },
+            (10, 2),
+            id='hbn_dataset_example',
+        ),
+        pytest.param(
+            {
+                'file': 'tests/gaze/io/files/sbsat_example.csv',
+                'time_column': 'time',
+                'pixel_columns': ['x_left', 'y_left'],
+                'read_csv_kwargs': {'separator': '\t'},
+            },
+            (10, 5),
+            id='sbsat_dataset_example',
+        ),
+        pytest.param(
+            {
+                'file': 'tests/gaze/io/files/gazebase_example.csv',
+                'time_column': 'n',
+                'position_columns': ['x', 'y'],
+                'read_csv_kwargs': {'null_values': 'NaN'},
+            },
+            (10, 7),
+            id='gazebase_dataset_example',
+        ),
+        pytest.param(
+            {
+                'file': 'tests/gaze/io/files/gaze_on_faces_example.csv',
+                'time_column': None,
+                'pixel_columns': ['x', 'y'],
+                'read_csv_kwargs': {
+                                    'separator': ',',
+                                    'has_header': False,
+                                    'new_columns': ['x', 'y'],
+                                    'dtypes': [pl.Float32, pl.Float32],
+                                    },
+            },
+            (10, 1),
+            id='gaze_on_faces_dataset_example',
+        ),
+        pytest.param(
+            {
+                'file': 'tests/gaze/io/files/gazebase_vr_example.csv',
+                'time_column': 'n',
+                'position_columns': ['lx', 'ly', 'rx', 'ry', 'x', 'y'],
+            },
+            (10, 11),
+            id='gazebase_vr_dataset_example',
+        ),
+        pytest.param(
+            {
+                'file': 'tests/gaze/io/files/judo1000_example.csv',
+                'time_column': 'time',
+                'pixel_columns': ['x_left', 'y_left', 'x_right', 'y_right'],
+                'read_csv_kwargs': {'separator': '\t'},
+            },
+            (10, 4),
+            id='judo1000_dataset_example',
+        ),
     ],
 )
 def test_shapes(kwargs, shape):
