3c8a7595c818ab8b8aae0de2c2a5cd22abc47252
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-10-10 14:06:40 +0200

    fix functional test

--- tests/functional/gaze_file_processing_test.py
+++ tests/functional/gaze_file_processing_test.py
@@ -120,7 +120,7 @@ def test_gaze_file_processing(gaze_from_kwargs):
     elif file_extension in {'.feather', '.ipc'}:
         gaze = pm.gaze.from_ipc(**gaze_from_kwargs)
     elif file_extension == '.asc':
-        gaze = pm.gaze.from_eyelink_asc(**gaze_from_kwargs)
+        gaze = pm.gaze.from_asc(**gaze_from_kwargs)
 
     assert gaze is not None
 
