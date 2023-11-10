95c888f4b1aba05bc3e479d14b06b9c8513a2d2e
Josephine Funken <funken@uni-bremen.de>
2023-11-10 11:50:19 +0100

    Error handling

--- src/pymovements/gaze/experiment.py
+++ src/pymovements/gaze/experiment.py
@@ -134,13 +134,13 @@ class Experiment:
     @property
     def sampling_rate(self) -> float:
         """Assign sampling rate to experiment."""
-        if self._sampling_rate is not None:
-            return self._sampling_rate
+        try:
+            if self._sampling_rate is not None:
+                return self._sampling_rate
 
-        if self.eyetracker is not None:
             return self.eyetracker.sampling_rate
-
-        raise TypeError("'self._sampling_rate' and 'self.eyetracker' must not both be None")
+        except:
+            print("'_sampling_rate' and 'eyetracker must not both be None")
 
     def pos2vel(
             self,
