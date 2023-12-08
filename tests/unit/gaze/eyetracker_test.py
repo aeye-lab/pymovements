fca8a04562277a5ac2ecb677645b888bf83033ce
Josephine Funken <funken@uni-bremen.de>
2023-12-08 10:58:39 +0100

    Added test for Eyetracker without sampling rate

--- tests/unit/gaze/eyetracker_test.py
+++ tests/unit/gaze/eyetracker_test.py
@@ -36,3 +36,11 @@ def test_eyetracker_with_negative_sampling_rate():
             -500.0, False, True, 'EyeLink 1000 Plus',
             '1.5.3', 'Arm Mount / Monocular / Remote',
         )
+
+
+def test_eyetracker_without_sampling_rate():
+    with pytest.raises(TypeError):
+        EyeTracker(
+            None, False, True, 'EyeLink 1000 Plus',
+            '1.5.3', 'Arm Mount / Monocular / Remote',
+        )
