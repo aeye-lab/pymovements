87e68c70a37493519b29e600f8d2638aa52b9175
Daniel G. Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2024-02-22 12:11:16 +0100

    Update __init__.py

--- src/pymovements/gaze/__init__.py
+++ src/pymovements/gaze/__init__.py
@@ -29,17 +29,6 @@
     pymovements.gaze.Screen
     pymovements.gaze.GazeDataFrame
 
-.. rubric:: Integration and IO
-
-.. autosummary::
-    :toctree:
-
-    pymovements.gaze.integration.from_numpy
-    pymovements.gaze.integration.from_pandas
-    pymovements.gaze.io.from_asc
-    pymovements.gaze.io.from_csv
-    pymovements.gaze.io.from_ipc
-
 .. rubric:: Transformations
 
 .. autosummary::
@@ -52,6 +41,23 @@
    pymovements.gaze.transforms.pos2acc
    pymovements.gaze.transforms.pos2vel
    pymovements.gaze.transforms.savitzky_golay
+
+.. rubric:: Input / Output
+
+.. autosummary::
+    :toctree:
+
+    pymovements.gaze.io.from_asc
+    pymovements.gaze.io.from_csv
+    pymovements.gaze.io.from_ipc
+
+.. rubric:: Integration
+
+.. autosummary::
+    :toctree:
+
+    pymovements.gaze.integration.from_numpy
+    pymovements.gaze.integration.from_pandas
 
 .. rubric:: Numpy Transformations
 
