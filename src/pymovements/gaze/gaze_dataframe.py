a663f0a2b1302325ac0a1b87d15727b38574827a
Jakob Chwastek <jakob.chwastek@gmail.com>
2024-03-14 19:02:07 +0100

    Remove sampling rate None Check

--- src/pymovements/gaze/gaze_dataframe.py
+++ src/pymovements/gaze/gaze_dataframe.py
@@ -1110,10 +1110,6 @@ class GazeDataFrame:
             if self.experiment is None:
                 raise ValueError("experiment must be specified if time_unit is 'step'")
 
-            if self.experiment.sampling_rate is None:
-                raise ValueError(
-                    "experiment must have a sampling rate specified if time_unit is 'step'",
-                )
 
             self.frame = self.frame.with_columns(
                 pl.col('time').mul(1000).truediv(self.experiment.sampling_rate),
