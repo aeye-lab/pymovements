4e943eca66296dd5c86f102ca00e1d2d96679ac3
Daniel G. Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2024-03-28 10:51:29 +0100

    pylint ignore not needed anymore

--- src/pymovements/datasets/toy_dataset.py
+++ src/pymovements/datasets/toy_dataset.py
@@ -92,7 +92,6 @@ class ToyDataset(DatasetDefinition):
     """
 
     # pylint: disable=similarities
-    # pylint: disable=too-many-instance-attributes
     # The PublicDatasetDefinition child classes potentially share code chunks for definitions.
 
     name: str = 'ToyDataset'
