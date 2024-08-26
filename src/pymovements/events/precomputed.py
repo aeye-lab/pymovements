caed462570fcb1eff8e17278e72f86196da87041
SiQube <reich.davidr@gmail.com>
2024-08-26 16:14:05 +0200

    docs: update docstring in precomuted events

--- src/pymovements/events/precomputed.py
+++ src/pymovements/events/precomputed.py
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Module for the TextDataFrame."""
+"""Module for the precomputed events dataframe."""
 from __future__ import annotations
 
 import polars as pl
