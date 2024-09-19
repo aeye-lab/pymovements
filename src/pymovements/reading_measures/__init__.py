070b2e4dfb3e5e4f9ac9c23073526139c06c0524
SiQube <reich.davidr@gmail.com>
2024-09-19 10:22:32 +0200

    hotfix: docstring

--- src/pymovements/reading_measures/__init__.py
+++ src/pymovements/reading_measures/__init__.py
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Provides access to stimulus classes.
+"""Provides access to reading measure classes.
 
 .. rubric:: Classes
 
