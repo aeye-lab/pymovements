e216133245ff2d79e117d68133c4ff8c9cd41b1d
David R. Reich <43832476+SiQube@users.noreply.github.com>
2024-09-27 12:47:18 +0200

    update docstring of fakenews dataset definition (#824)

--- src/pymovements/datasets/fakenews.py
+++ src/pymovements/datasets/fakenews.py
@@ -44,31 +44,54 @@ class FakeNewsPerception(DatasetDefinition):
 
     Attributes
     ----------
-    name : str
+    name: str
         The name of the dataset.
-    mirrors : tuple[str, ...]
+    has_files: dict[str, bool]
+        Indicate whether the dataset contains 'gaze', 'precomputed_events', and
+        'precomputed_reading_measures'.
+    mirrors: dict[str, tuple[str, ...]]
         A tuple of mirrors of the dataset. Each entry must be of type `str` and end with a '/'.
-    resources : tuple[dict[str, str], ...]
+    resources: dict[str, tuple[dict[str, str], ...]]
         A tuple of dataset gaze_resources. Each list entry must be a dictionary with the following
         keys:
         - `resource`: The url suffix of the resource. This will be concatenated with the mirror.
         - `filename`: The filename under which the file is saved as.
         - `md5`: The MD5 checksum of the respective file.
-    experiment : Experiment
+    experiment: Experiment
         The experiment definition.
-    filename_format : str
+    extract: dict[str, bool]
+        Decide whether to extract the data.
+    filename_format: dict[str, str]
         Regular expression which will be matched before trying to load the file. Namedgroups will
         appear in the `fileinfo` dataframe.
-    filename_format_dtypes : dict[str, type], optional
+    filename_format_dtypes: dict[str, dict[str, type]]
         If named groups are present in the `filename_format`, this makes it possible to cast
         specific named groups to a particular datatype.
-    column_map : dict[str, str]
+    trial_columns: list[str]
+            The name of the trial columns in the input data frame. If the list is empty or None,
+            the input data frame is assumed to contain only one trial. If the list is not empty,
+            the input data frame is assumed to contain multiple trials and the transformation
+            methods will be applied to each trial separately.
+    time_column: str
+        The name of the timestamp column in the input data frame. This column will be renamed to
+        ``time``.
+    time_unit: str
+        The unit of the timestamps in the timestamp column in the input data frame. Supported
+        units are 's' for seconds, 'ms' for milliseconds and 'step' for steps. If the unit is
+        'step' the experiment definition must be specified. All timestamps will be converted to
+        milliseconds.
+    pixel_columns: list[str]
+        The name of the pixel position columns in the input data frame. These columns will be
+        nested into the column ``pixel``. If the list is empty or None, the nested ``pixel``
+        column will not be created.
+    column_map: dict[str, str]
         The keys are the columns to read, the values are the names to which they should be renamed.
-    custom_read_kwargs : dict[str, Any], optional
+    custom_read_kwargs: dict[str, Any]
         If specified, these keyword arguments will be passed to the file reading function.
     """
 
     name: str = 'FakeNewsPerception'
+
     has_files: dict[str, bool] = field(
         default_factory=lambda: {
             'gaze': False,
@@ -76,12 +99,13 @@ class FakeNewsPerception(DatasetDefinition):
             'precomputed_reading_measures': False,
         },
     )
-    extract: dict[str, bool] = field(default_factory=lambda: {'precomputed_events': True})
+
     mirrors: dict[str, tuple[str, ...]] = field(
         default_factory=lambda: {
             'precomputed_events': ('https://doi.org/10.7910/DVN/C1UD2A',),
         },
     )
+
     resources: dict[str, tuple[dict[str, str], ...]] = field(
         default_factory=lambda: {
             'precomputed_events': (
@@ -93,6 +117,7 @@ class FakeNewsPerception(DatasetDefinition):
             ),
         },
     )
+
     experiment: Experiment = Experiment(
         screen_width_px=1920,
         screen_height_px=1080,
@@ -103,21 +128,30 @@ class FakeNewsPerception(DatasetDefinition):
         sampling_rate=600,
     )
 
+    extract: dict[str, bool] = field(default_factory=lambda: {'precomputed_events': True})
+
     filename_format: dict[str, str] = field(
         default_factory=lambda: {
             'precomputed_events': r'P{subject_id:d}_{session_id:d}_{truth_value:s}.csv',
         },
     )
+
     filename_format_dtypes: dict[str, dict[str, type]] = field(
         default_factory=lambda: {
             'precomputed_events': {'subject_id': int, 'session_id': int, 'truth_value': str},
         },
     )
+
     trial_columns: list[str] = field(default_factory=lambda: [])
+
     time_column: str = 'starttime'
+
     time_unit: str = 'milliseconds'
+
     pixel_columns: list[str] = field(default_factory=lambda: [])
+
     column_map: dict[str, str] = field(default_factory=lambda: {})
+
     custom_read_kwargs: dict[str, Any] = field(
         default_factory=lambda: {
             'precomputed_events': {
