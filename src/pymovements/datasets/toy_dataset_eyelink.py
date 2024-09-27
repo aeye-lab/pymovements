d3eecee9eb1da574c58631921793a25a7984d82e
David R. Reich <43832476+SiQube@users.noreply.github.com>
2024-09-27 14:22:46 +0200

    update docstring of toy_dataset_eyelink dataset definition (#834)

--- src/pymovements/datasets/toy_dataset_eyelink.py
+++ src/pymovements/datasets/toy_dataset_eyelink.py
@@ -48,14 +48,22 @@ class ToyDatasetEyeLink(DatasetDefinition):
     name: str
         The name of the dataset.
 
+    has_files: dict[str, bool]
+        Indicate whether the dataset contains 'gaze', 'precomputed_events', and
+        'precomputed_reading_measures'.
+
     mirrors: dict[str, tuple[str, ...]]
         A tuple of mirrors of the dataset. Each entry must be of type `str` and end with a '/'.
 
     resources: dict[str, tuple[dict[str, str], ...]]
-        A tuple of dataset resources. Each list entry must be a dictionary with the following keys:
+        A tuple of dataset gaze_resources. Each list entry must be a dictionary with the following
+        keys:
         - `resource`: The url suffix of the resource. This will be concatenated with the mirror.
         - `filename`: The filename under which the file is saved as.
         - `md5`: The MD5 checksum of the respective file.
+
+    extract: dict[str, bool]
+        Decide whether to extract the data.
 
     experiment: Experiment
         The experiment definition.
@@ -67,6 +75,27 @@ class ToyDatasetEyeLink(DatasetDefinition):
     filename_format_dtypes: dict[str, dict[str, type]]
         If named groups are present in the `filename_format`, this makes it possible to cast
         specific named groups to a particular datatype.
+
+    trial_columns: list[str]
+            The name of the trial columns in the input data frame. If the list is empty or None,
+            the input data frame is assumed to contain only one trial. If the list is not empty,
+            the input data frame is assumed to contain multiple trials and the transformation
+            methods will be applied to each trial separately.
+
+    time_column: str
+        The name of the timestamp column in the input data frame. This column will be renamed to
+        ``time``.
+
+    time_unit: str
+        The unit of the timestamps in the timestamp column in the input data frame. Supported
+        units are 's' for seconds, 'ms' for milliseconds and 'step' for steps. If the unit is
+        'step' the experiment definition must be specified. All timestamps will be converted to
+        milliseconds.
+
+    pixel_columns: list[str]
+        The name of the pixel position columns in the input data frame. These columns will be
+        nested into the column ``pixel``. If the list is empty or None, the nested ``pixel``
+        column will not be created.
 
     column_map: dict[str, str]
         The keys are the columns to read, the values are the names to which they should be renamed.
@@ -104,6 +133,7 @@ class ToyDatasetEyeLink(DatasetDefinition):
             'precomputed_reading_measures': False,
         },
     )
+
     mirrors: dict[str, tuple[str, ...]] = field(
         default_factory=lambda: {
             'gaze': (
@@ -124,6 +154,7 @@ class ToyDatasetEyeLink(DatasetDefinition):
                 ),
         },
     )
+
     extract: dict[str, bool] = field(default_factory=lambda: {'gaze': True})
 
     experiment: Experiment = Experiment(
@@ -157,8 +188,6 @@ class ToyDatasetEyeLink(DatasetDefinition):
         },
     )
 
-    column_map: dict[str, str] = field(default_factory=lambda: {})
-
     trial_columns: list[str] = field(default_factory=lambda: ['subject_id', 'session_id'])
 
     time_column: str = 'time'
@@ -166,6 +195,8 @@ class ToyDatasetEyeLink(DatasetDefinition):
     time_unit: str = 'ms'
 
     pixel_columns: list[str] = field(default_factory=lambda: ['x_pix', 'y_pix'])
+
+    column_map: dict[str, str] = field(default_factory=lambda: {})
 
     custom_read_kwargs: dict[str, dict[str, Any]] = field(
         default_factory=lambda: {
