9acb9263341a8ce95a2d0e7804dea06a96414c22
saphjra <49561526+saphjra@users.noreply.github.com>
2024-11-20 14:07:39 +0100

    changed inconsistency_check to include a print statement. Probably not the cleanest way to do it

--- tests/unit/utils/parsing_test.py
+++ tests/unit/utils/parsing_test.py
@@ -17,7 +17,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 # Copyright (c) 2023-2024 The pymovements Project Authors
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
