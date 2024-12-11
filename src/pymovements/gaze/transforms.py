3b00108ab2aae05ef14fd8a4600722e1de9369f2
shubi (nlp-srv2) <omer.shubi@gmail.com>
2024-12-11 11:40:04 +0200

    Revert "collections import fix"
    This reverts commit 267d0eee41ff6db34cea4cec6832f4dd392d6cb2.

--- src/pymovements/gaze/transforms.py
+++ src/pymovements/gaze/transforms.py
@@ -20,10 +20,7 @@
 """Module for py:func:`pymovements.gaze.transforms."""
 from __future__ import annotations
 
-try:
-    from collections.abc import Callable  # noqa
-except ImportError:
-    from collections import Callable  # noqa
+from collections.abc import Callable
 from functools import partial
 from typing import Any
 from typing import TypeVar
