5dc650c972f20d8928fc472b0d8009da36aca54a
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-01-22 11:53:28 +0100

    adjust docstring of private function

--- src/pymovements/gaze/gaze_dataframe.py
+++ src/pymovements/gaze/gaze_dataframe.py
@@ -1457,6 +1457,13 @@ class GazeDataFrame:
 
 
 def _check_trial_columns(trial_columns: list[str] | None, data: pl.DataFrame) -> None:
+    """Checks trial_columns for integrity.
+
+    Parameters
+    ----------
+    trial_columns: The name of the trial columns in the input data frame.
+    data: The dataframe which holds the columns.
+    """
     if trial_columns:
         # Make sure there are no duplicates in trial_columns, else polars raises DuplicateError.
         if len(set(trial_columns)) != len(trial_columns):
