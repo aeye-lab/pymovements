0939c85930b4cca42a9e1a5620ba94fc731c2bc7
Daniel G. Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-01-22 12:27:42 +0100

    add comments

--- src/pymovements/dataset/dataset_files.py
+++ src/pymovements/dataset/dataset_files.py
@@ -309,9 +309,10 @@ def load_gaze_file(
         [column for column in fileinfo_row.keys() if column != 'filepath']
     }
 
+    # check if we have any trial or fileinfo columns specified.
     if not definition.trial_columns and not fileinfo_columns:
         trial_columns = None
-    else:
+    else:  # at least one element exists, check for duplicates and merge columns
         if not definition.trial_columns:
             trial_columns = []
         else:
