4d84ce5c70b808193162269f558f94cf3126f4e6
David R. Reich <43832476+SiQube@users.noreply.github.com>
2025-02-25 19:53:10 +0100

    dataset: add Provo corpus (#959)

--- src/pymovements/datasets/__init__.py
+++ src/pymovements/datasets/__init__.py
@@ -39,6 +39,7 @@
     pymovements.datasets.InteRead
     pymovements.datasets.JuDo1000
     pymovements.datasets.PoTeC
+    pymovements.datasets.Provo
     pymovements.datasets.SBSAT
 
 
@@ -66,6 +67,7 @@ from pymovements.datasets.hbn import HBN
 from pymovements.datasets.interead import InteRead
 from pymovements.datasets.judo1000 import JuDo1000
 from pymovements.datasets.potec import PoTeC
+from pymovements.datasets.provo import Provo
 from pymovements.datasets.sb_sat import SBSAT
 from pymovements.datasets.toy_dataset import ToyDataset
 from pymovements.datasets.toy_dataset_eyelink import ToyDatasetEyeLink
@@ -87,6 +89,7 @@ __all__ = [
     'InteRead',
     'JuDo1000',
     'PoTeC',
+    'Provo',
     'SBSAT',
     'ToyDataset',
     'ToyDatasetEyeLink',
