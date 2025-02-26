cab2674c2a65730ee36599afc42f8eb8cb343cdd
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-02-26 12:16:50 +0100

    add tests for deprecation

---
+++ tests/unit/utils/parsing_test.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2025 The pymovements Project Authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Tests deprecated utils.parsing."""
+import re
+
+import pytest
+
+import pymovements as pm
+
+
+def test_is_parsing_deprecated():
+    filepath = 'tests/files/eyelink_monocular_example.asc'
+
+    with pytest.raises(DeprecationWarning):
+        _ = pm.utils.parsing.parse_eyelink(filepath)
+
+
+def test_is_parsing_removed():
+    filepath = 'tests/files/eyelink_monocular_example.asc'
+
+    with pytest.raises(DeprecationWarning) as info:
+        _ = pm.utils.parsing.parse_eyelink(filepath)
+
+    regex = re.compile(r'.*will be removed in v(?P<version>[0-9]*[.][0-9]*[.][0-9]*)[.)].*')
+
+    msg = info.value.args[0]
+    remove_version = regex.match(msg).groupdict()['version']
+    current_version = pm.__version__.split('+')[0]
+    assert current_version < remove_version, (
+        f'utils/parsing.py was planned to be removed in v{remove_version}. '
+        f'Current version is v{current_version}.'
+    )
