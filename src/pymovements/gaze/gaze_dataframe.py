b1021e39349a1c889f1bbce19c3b12acdc09db07
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-03-06 20:44:17 +0100

    update docstrings

--- src/pymovements/gaze/gaze_dataframe.py
+++ src/pymovements/gaze/gaze_dataframe.py
@@ -65,7 +65,7 @@ class GazeDataFrame:
         The unit of the timestamps in the timestamp column in the input data frame. Supported
         units are 's' for seconds, 'ms' for milliseconds and 'step' for steps. If the unit is
         'step' the experiment definition must be specified. All timestamps will be converted to
-        milliseconds. (default: 'ms')
+        milliseconds. If time_unit is None, milliseconds are assumed. (default: None)
     pixel_columns:list[str] | None
         The name of the pixel position columns in the input data frame. These columns will be
         nested into the column ``pixel``. If the list is empty or None, the nested ``pixel``
