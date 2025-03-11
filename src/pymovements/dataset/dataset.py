9fb9f35100d04f7aba46096d6be922c6a8e0b1fb
MehrazRumman <stardark394@gmail.com>
2025-03-11 14:10:23 +0600

    load_gaze_files updated

--- src/pymovements/dataset/dataset.py
+++ src/pymovements/dataset/dataset.py
@@ -189,7 +189,8 @@ class Dataset:
             This argument is used only for this single call and does not alter
             :py:meth:`pymovements.Dataset.preprocessed_rootpath`. (default: None)
         extension: str
-            Specifies the file format for loading data. Valid options are: `csv`, `feather`.
+            Specifies the file format for loading data. Valid options are:`csv`, `feather`,
+            `tsv`, `txt`, `asc`.
             (default: 'feather')
 
         Returns
