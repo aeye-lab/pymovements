cd2c6b4430d4a840d1282ec748ea7e8b126b1972
SiQube <reich.davidr@gmail.com>
2025-03-14 14:24:44 +0100

    remove  from dataset

--- src/pymovements/dataset/dataset.py
+++ src/pymovements/dataset/dataset.py
@@ -1110,13 +1110,3 @@ class Dataset:
             raise AttributeError('gaze files were not loaded yet. please run load() beforehand')
         if len(self.gaze) == 0:
             raise AttributeError('no files present in gaze attribute')
-
-    def to_yaml(self, yaml_path: str | Path) -> None:
-        """Write the dataset definition to a YAML file.
-
-        Parameters
-        ----------
-        yaml_path: str | Path
-            Path where to save the YAML file to.
-        """
-        self.definition.to_yaml(yaml_path)
