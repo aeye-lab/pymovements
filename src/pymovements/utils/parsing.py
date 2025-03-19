5e1389c64ea3df4f63fb9ae9c2094889cdb95654
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-03-19 19:45:21 +0100

    Update parsing.py

--- src/pymovements/utils/parsing.py
+++ src/pymovements/utils/parsing.py
@@ -19,7 +19,7 @@
 # SOFTWARE.
 """Module for parsing input data.
 
-.. deprecated:: v0.21.1
+.. deprecated:: v0.21.0
    Please use :py:func:`~pymovements.gaze.from_asc()` instead.
    This module will be removed in v0.26.0.
 """
@@ -37,7 +37,7 @@ from pymovements.gaze._utils.parsing import parse_eyelink as _parse_eyelink
 @deprecated(
     reason='Please use gaze.from_asc() instead. '
            'This module will be removed in v0.26.0.',
-    version='v0.21.1',
+    version='v0.21.0',
 )
 def parse_eyelink(
         filepath: Path | str,
@@ -48,7 +48,7 @@ def parse_eyelink(
 ) -> tuple[pl.DataFrame, dict[str, Any]]:
     """Parse EyeLink asc file.
 
-    .. deprecated:: v0.21.1
+    .. deprecated:: v0.21.0
        Please use :py:func:`~pymovements.gaze.from_asc()` instead.
        This module will be removed in v0.26.0.
 
