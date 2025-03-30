fc2d9fc920285480f761d756e6947dfe847ae912
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-03-30 13:57:52 +0200

    adjust docstring

--- src/pymovements/datasets/toy_dataset_eyelink.py
+++ src/pymovements/datasets/toy_dataset_eyelink.py
@@ -74,7 +74,7 @@ class ToyDatasetEyeLink(DatasetDefinition):
         If named groups are present in the `filename_format`, this makes it possible to cast
         specific named groups to a particular datatype.
 
-    trial_columns: list[str]
+    trial_columns: list[str] | None
             The name of the trial columns in the input data frame. If the list is empty or None,
             the input data frame is assumed to contain only one trial. If the list is not empty,
             the input data frame is assumed to contain multiple trials and the transformation
