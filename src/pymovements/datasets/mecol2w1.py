047169dac6c7570b2dd36fa89f13f2b08b42a635
SiQube <reich.davidr@gmail.com>
2025-04-17 05:55:03 +0200

    add docstring topublic module

--- src/pymovements/datasets/mecol2w1.py
+++ src/pymovements/datasets/mecol2w1.py
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+"""Provides a definition for the MECOL2W1 dataset."""
 from __future__ import annotations
 
 from dataclasses import dataclass
