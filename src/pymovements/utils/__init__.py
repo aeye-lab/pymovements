4332e7359560b62f2cb763ce8225f0ffe39f3e05
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-04-25 12:34:52 +0200

    Update __init__.py

--- src/pymovements/utils/__init__.py
+++ src/pymovements/utils/__init__.py
@@ -20,8 +20,8 @@
 # pylint: disable=cyclic-import
 """Provides utility functions.
 
-.. deprecated:: v0.21.1
-   This module will be removed in v0.26.0.
+.. deprecated:: v0.22.0
+   This module will be removed in v0.27.0.
 
 .. rubric:: Modules
 
