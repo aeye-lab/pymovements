3dd7114ba1d307bb3555feaa90f9d85a87cc4ca2
Andreas Säuberli <andreas.saeuberli@gmail.com>
2025-04-27 17:11:28 +0200

    Update schema tests

--- tests/unit/dataset/dataset_test.py
+++ tests/unit/dataset/dataset_test.py
@@ -901,7 +901,11 @@ def test_detect_events_auto_eye(detect_event_kwargs, gaze_dataset_configuration)
     dataset.detect_events(**detect_event_kwargs)
 
     expected_schema = {
-        'subject_id': pl.Int64, **events.EventDataFrame._minimal_schema, 'duration': pl.Int64,
+        'subject_id': pl.Int64,
+        'name': pl.Utf8,
+        'onset': pl.Int64,
+        'offset': pl.Int64,
+        'duration': pl.Int64,
     }
     for result_event_df in dataset.events:
         assert result_event_df.schema == expected_schema
@@ -945,7 +949,9 @@ def test_detect_events_explicit_eye(detect_event_kwargs, gaze_dataset_configurat
 
         expected_schema = {
             'subject_id': pl.Int64,
-            **events.EventDataFrame._minimal_schema,
+            'name': pl.Utf8,
+            'onset': pl.Int64,
+            'offset': pl.Int64,
             'duration': pl.Int64,
         }
 
@@ -973,7 +979,9 @@ def test_detect_events_explicit_eye(detect_event_kwargs, gaze_dataset_configurat
             },
             {
                 'subject_id': pl.Int64,
-                **events.EventDataFrame._minimal_schema,
+                'name': pl.Utf8,
+                'onset': pl.Int64,
+                'offset': pl.Int64,
                 'duration': pl.Int64,
             },
             id='two-saccade-runs',
@@ -991,7 +999,9 @@ def test_detect_events_explicit_eye(detect_event_kwargs, gaze_dataset_configurat
             },
             {
                 'subject_id': pl.Int64,
-                **events.EventDataFrame._minimal_schema,
+                'name': pl.Utf8,
+                'onset': pl.Int64,
+                'offset': pl.Int64,
                 'duration': pl.Int64,
             },
             id='one-saccade-one-fixation-run',
@@ -1697,7 +1707,9 @@ def test_event_dataframe_add_property_has_expected_height(
             {'event_properties': 'peak_velocity'},
             {
                 'subject_id': pl.Int64,
-                **events.EventDataFrame._minimal_schema,
+                'name': pl.Utf8,
+                'onset': pl.Int64,
+                'offset': pl.Int64,
                 'duration': pl.Int64,
                 'peak_velocity': pl.Float64,
             },
@@ -1707,7 +1719,9 @@ def test_event_dataframe_add_property_has_expected_height(
             {'event_properties': 'location'},
             {
                 'subject_id': pl.Int64,
-                **events.EventDataFrame._minimal_schema,
+                'name': pl.Utf8,
+                'onset': pl.Int64,
+                'offset': pl.Int64,
                 'duration': pl.Int64,
                 'location': pl.List(pl.Float64),
             },
