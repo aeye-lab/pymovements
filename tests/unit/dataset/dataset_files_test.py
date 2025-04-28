d754b18a5f6f43acb2c71abb67d2efc675545673
siqube <reich.davidr@gmail.com>
2025-04-28 14:14:48 +0000

    incorporate non-breaking changes

--- tests/unit/dataset/dataset_files_test.py
+++ tests/unit/dataset/dataset_files_test.py
@@ -274,7 +274,8 @@ def test_load_precomputed_file_unsupported_file_format():
         pm.dataset.dataset_files.load_precomputed_event_file(filepath)
 
     msg, = exc.value.args
-    assert msg == 'unsupported file format ".feather". Supported formats are: .csv, .rda, .tsv, .txt'  # noqa: E501
+    assert msg == 'unsupported file format ".feather". '\
+        'Supported formats are: .csv, .rda, .tsv, .txt'
 
 
 def test_load_precomputed_file_rda_raise_value_error():
