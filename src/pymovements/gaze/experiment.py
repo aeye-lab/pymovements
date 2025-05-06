50f1fdb6efed490f37d32d8e7de54d5474696c7f
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-05-06 18:09:47 +0200

    reformat output to original formatting of Yanis750

--- src/pymovements/gaze/experiment.py
+++ src/pymovements/gaze/experiment.py
@@ -160,16 +160,10 @@ class Experiment:
         ...     "sampling_rate": 1000.0,
         ... })
         >>> print(experiment)
-        Experiment(
-            screen=Screen(
-                width_px=1280, height_px=1024, width_cm=38.0, height_cm=30.0,
-                distance_cm=68.0, origin='upper left'
-            ),
-            eyetracker=EyeTracker(
-                sampling_rate=1000.0, left=None, right=None,
-                model=None, version=None, vendor=None, mount=None
-            )
-        )
+        Experiment(screen=Screen(width_px=1280, height_px=1024, width_cm=38.0, height_cm=30.0,
+                                 distance_cm=68.0, origin='upper left'),
+                   eyetracker=EyeTracker(sampling_rate=1000.0, left=None, right=None,
+                                        model=None, version=None, vendor=None, mount=None))
 
         The same result using nested dictionaries for `screen` and `eyetracker`:
 
@@ -187,16 +181,10 @@ class Experiment:
         ...     }
         ... })
         >>> print(experiment)
-        Experiment(
-            screen=Screen(
-                width_px=1280, height_px=1024, width_cm=38.0, height_cm=30.0,
-                distance_cm=68.0, origin='upper left'
-            ),
-            eyetracker=EyeTracker(
-                sampling_rate=1000.0, left=None, right=None,
-                model=None, version=None, vendor=None, mount=None
-            )
-        )
+        Experiment(screen=Screen(width_px=1280, height_px=1024, width_cm=38.0, height_cm=30.0,
+                                 distance_cm=68.0, origin='upper left'),
+                   eyetracker=EyeTracker(sampling_rate=1000.0, left=None, right=None,
+                                        model=None, version=None, vendor=None, mount=None))
 
         Returns
         -------
