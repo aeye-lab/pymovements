149d64a700f23005c826b4325a66fa6ff1663d8e
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-05-09 23:59:36 +0200

    ci: add pre-commit hook to write datasets.yaml

---
+++ .pre-commit/write_datasets_yaml.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2025 The pymovements Project Authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+from __future__ import annotations
+
+from pathlib import Path
+
+import yaml
+
+
+def main(
+        datasets_dirpath: str | Path = './src/pymovements/datasets',
+        datasets_yaml_filename: str = 'datasets.yaml',
+) -> int:
+    if isinstance(datasets_dirpath, str):
+        datasets_dirpath = Path(datasets_dirpath)
+
+    dataset_filename_stems = sorted(
+        [
+            filepath.stem for filepath in datasets_dirpath.glob('*.yaml')
+            if filepath.name != datasets_yaml_filename  # Ignore datasets yaml file.
+        ],
+    )
+
+    with open(datasets_dirpath / datasets_yaml_filename) as f:
+        dataset_yaml_content = yaml.safe_load(f)
+
+    # File content looks good. Exit successfully.
+    if dataset_filename_stems == dataset_yaml_content:
+        return 0
+
+    # We have some updates in the datasets directory. Update the datasets yaml file.
+    with open(datasets_dirpath / datasets_yaml_filename, 'w') as f:
+        yaml.dump(dataset_filename_stems, f)
+
+    return 1
+
+
+if __name__ == '__main__':
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--path')
+    # args = parser.parse_args()
+    raise SystemExit(main())
