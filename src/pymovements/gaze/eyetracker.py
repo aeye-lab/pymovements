070d0f4385e2fe3c6a14e32cdaa638ec44fbd62d
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-05-13 18:10:42 +0200

    cosmetic changes in eyetracker.py

--- src/pymovements/gaze/eyetracker.py
+++ src/pymovements/gaze/eyetracker.py
@@ -87,8 +87,8 @@ class EyeTracker:
         ----------
         exclude_none: bool
             Exclude attributes that are either ``None`` or that are objects that evaluate to
-            ``False`` (e.g., ``[], {}, EyeTracker()``). Attributes of type ``bool``, ``int``,
-            and ``float`` are not excluded.
+            ``False`` (e.g., ``[]``, ``{}``, ``EyeTracker()``). Attributes of type ``bool``,
+            ``int``, and ``float`` are not excluded.
 
         Returns
         -------
@@ -106,5 +106,5 @@ class EyeTracker:
         return _dict
 
     def __bool__(self) -> bool:
-        """Return True if the experiment has data, else False."""
+        """Return True if the eyetracker has data defined, else False."""
         return not all(not value for value in self.__dict__.values())
