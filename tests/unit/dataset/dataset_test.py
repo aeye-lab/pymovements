e7992d3a68bfbd70a43cab14cc5e04d50909ecb7
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-05-21 19:57:59 +0200

    adjust more tests

--- tests/unit/dataset/dataset_test.py
+++ tests/unit/dataset/dataset_test.py
@@ -1929,7 +1929,6 @@ def precomputed_fixture_dataset(request, tmp_path):
                 'precomputed_events': True,
                 'precomputed_reading_measures': False,
             },
-            extract={'precomputed_events': False},
             filename_format_schema_overrides={'precomputed_events': {}},
         )
     else:
@@ -2001,7 +2000,6 @@ def precomputed_rm_fixture_dataset(request, tmp_path):
                 'precomputed_events': False,
                 'precomputed_reading_measures': True,
             },
-            extract={'precomputed_reading_measures': False},
             filename_format_schema_overrides={
                 'precomputed_events': {},
                 'precomputed_reading_measures': {},
