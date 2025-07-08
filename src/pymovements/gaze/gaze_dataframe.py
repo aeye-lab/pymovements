b435f9b7beb71152e59eb98dba75bf00588cac32
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-07-08 18:03:50 +0200

    typo

--- src/pymovements/gaze/gaze_dataframe.py
+++ src/pymovements/gaze/gaze_dataframe.py
@@ -970,7 +970,7 @@ class GazeDataFrame:
         --------
         Let's initialize an example GazeDataFrame first:
         >>> gaze = pm.gaze.from_numpy(
-        ...     pixels=np.concatenate([np.zeros((40, 2)), np.full((10, 2), np.nan), np.ones((50, 2))]),
+        ...     pixel=np.concatenate([np.zeros((40, 2)), np.full((10, 2), np.nan), np.ones((50, 2))]),
         ... )
 
         You can calculate measures, for example the null ratio like this:
