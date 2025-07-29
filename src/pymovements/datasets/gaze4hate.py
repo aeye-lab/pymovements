c10bb95307fe9cb7ff89c95b1922dcae28d44c5d
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-07-29 15:31:12 +0200

    Update gaze4hate.py

--- src/pymovements/datasets/gaze4hate.py
+++ src/pymovements/datasets/gaze4hate.py
@@ -25,6 +25,7 @@ from dataclasses import field
 from typing import Any
 
 from pymovements.dataset.dataset_definition import DatasetDefinition
+from pymovements.dataset.resources import ResourceDefinitions
 from pymovements.gaze.experiment import Experiment
 
 
@@ -50,7 +51,7 @@ class Gaze4Hate(DatasetDefinition):
         Indicate whether the dataset contains 'gaze', 'precomputed_events', and
         'precomputed_reading_measures'.
 
-    resources: dict[str, list[dict[str, str]]]
+    resources: ResourceDefinitions
         A list of dataset gaze_resources. Each list entry must be a dictionary with the following
         keys:
         - `resource`: The url suffix of the resource. This will be concatenated with the mirror.
@@ -110,8 +111,8 @@ class Gaze4Hate(DatasetDefinition):
         },
     )
 
-    resources: dict[str, list[dict[str, str]]] = field(
-        default_factory=lambda:
+resources: ResourceDefinitions = field(
+        default_factory=lambda: ResourceDefinitions.from_dict(
             {
                 'precomputed_events': [
                     {
@@ -128,6 +129,7 @@ class Gaze4Hate(DatasetDefinition):
                     },
                 ],
             },
+        ),
     )
 
     experiment: Experiment = field(
