05b6a0d3b00d041f1af4dce49a7fc5b87d90c7c4
Anastassia Shaitarova <38567830+shaitarAn@users.noreply.github.com>
2025-08-27 11:31:05 +0200

    feat: automatically set axis dimension in traceplot (#1247)
    Co-authored-by: Sasho Gramatikov <sasho.gramatikov@finki.ukim.mk>

--- src/pymovements/plotting/traceplot.py
+++ src/pymovements/plotting/traceplot.py
@@ -109,6 +109,20 @@ def traceplot(
     y_signal = gaze.samples[position_column].list.get(1)
     figsize = (10, 10)
 
+    screen_width_px = None
+    screen_height_px = None
+
+    if gaze.experiment is not None:
+        screen = gaze.experiment.screen
+        screen_width_px = screen.width_px
+        screen_height_px = screen.height_px
+
+        if screen.origin != 'upper left':
+            raise ValueError(
+                f"Origin of the experiment screen is set to {screen.origin}, "
+                "but only 'upper left' is supported for traceplot.",
+            )
+
     fig, ax, cmap, cmap_norm, cval, show_cbar = _setup_matplotlib(
         x_signal,
         y_signal,
@@ -133,6 +147,11 @@ def traceplot(
         cmap_norm,
         cval,
     )
+
+    # only set axes limits if we have experiment screen info
+    if screen_width_px is not None and screen_height_px is not None:
+        ax.set_xlim(0, screen_width_px)
+        ax.set_ylim(screen_height_px, 0)
 
     if show_cbar:
         # sm = matplotlib.cm.ScalarMappable(cmap=cmap, norm=cmap_norm)
