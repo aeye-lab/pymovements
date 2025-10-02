d9e11852e2e0264ad14e793872adf5f39554418a
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2025-10-02 14:40:22 +0200

    change fixture scope to session level

--- tests/fixtures/file_fixtures.py
+++ tests/fixtures/file_fixtures.py
@@ -24,13 +24,13 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture(name='testfiles_dirpath')
+@pytest.fixture(name='testfiles_dirpath', scope='session')
 def fixture_testfiles_dirpath(request):
     """Return the path to tests/files."""
     return request.config.rootpath / 'tests' / 'files'
 
 
-@pytest.fixture(name='make_example_file', scope='function')
+@pytest.fixture(name='make_example_file', scope='session')
 def fixture_make_example_file(testfiles_dirpath, tmp_path):
     """Make a copy of a file from one of the example files in tests/files.
 
